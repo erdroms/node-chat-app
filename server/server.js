4589b408b97761b7556ad094ba705a9b9f0ad8b4
Erin Droms <erin.droms@rtslabs.com>
2019-03-07 10:56:45 -0500

    add .gitignore, add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3002;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+// register event listener
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', (socket) => {
+    console.log('Client disconnected');
+  })
+})
+
+server.listen(port, () => {
   console.log('Server running on port 3002')
 });
